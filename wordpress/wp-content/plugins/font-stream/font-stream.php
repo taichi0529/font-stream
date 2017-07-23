0f2d051f8b314f7a745d9eef1773653452b44b7d
中村太一 <taichi@taichi.local>
2017-07-24 01:54:07 +0900

    update

--- wordpress/wp-content/plugins/font-stream/font-stream.php
+++ wordpress/wp-content/plugins/font-stream/font-stream.php
@@ -12,27 +12,41 @@ Change Log:
 20170717 v0.1.0 初版
 */
 
+
 $FontStream = new Font_Stream();
+add_action( 'wp_ajax_font_stream_save', [ $FontStream, 'save' ] );
+add_action( 'wp_ajax_font_stream_load', [ $FontStream, 'load' ] );
+
+add_action( 'wp_head', [ $FontStream, 'css' ], 1 );
+
 class Font_Stream
 {
+	protected $_option_name = 'comquest_font_stream_data';
 
 	public function __construct() {
 
-		add_action( 'admin_enqueue_scripts', function($hook) {
+		add_action( 'admin_enqueue_scripts', function ( $hook ) {
 			global $pagenow;
-			if ( $pagenow == "options-general.php" && isset($_GET['page']) && $_GET['page'] == "font_stream" )
-			{
-				wp_enqueue_script( "font-stream-js", plugins_url('font-stream/js/main.js'), ['jquery'] );
-				wp_enqueue_style( "font-stream-css", plugins_url('font-stream/css/main.css'));
+			if ( $pagenow == "options-general.php" && isset( $_GET['page'] ) && $_GET['page'] == "font_stream" ) {
+				wp_enqueue_script( "font-stream-js", plugins_url( 'font-stream/js/main.js' ), [ 'jquery' ] );
+				wp_localize_script(
+					'font-stream-js',
+					'FONT_STREAM',
+					[
+						'endpoint' => admin_url( 'admin-ajax.php' )
+					]
+				);
+				wp_enqueue_style( "font-stream-css", plugins_url( 'font-stream/css/main.css' ) );
 			}
 		} );
 
 		// メニューを追加します。
-		add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), array( $this, 'fontstream_edit_plugin_list_links' ));
+		add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ),
+			[ $this, 'fontstream_edit_plugin_list_links' ] );
 
-		add_action('admin_menu', function(){
-			add_options_page('fontstream', 'Font Stream', 'manage_options', 'font_stream', array($this, 'show_option'));
-		});
+		add_action( 'admin_menu', function () {
+			add_options_page( 'fontstream', 'Font Stream', 'manage_options', 'font_stream', [ $this, 'show_option' ] );
+		} );
 
 //		$this->set_option();
 	}
@@ -64,18 +78,53 @@ class Font_Stream
 		$master_on_checked = $this->font_stream_master_on === '1' ? 'checked' : '';
 
 		echo "<h2>Font Stream</h2>";
-		echo '<form action="'.admin_url( 'options-general.php?page=font_stream' ).'" method="post" accept-charset="utf-8" class="font-stream">
+		echo '<form action="' . admin_url( 'options-general.php?page=font_stream' ) . '" method="post" accept-charset="utf-8" class="font-stream">
 			<h4>Token</h4>
 			<div>
-				<input type="text" name="font_stream_directory" value="'.$this->font_stream_directory.'">
+				<input type="text" id="token" value="' . $this->font_stream_directory . '">
 			</div>
 			
 			<h4>Font</h4>
-			<ul class="fontList">
-				<li>アニト L 等幅 <a href="#">削除</a></li>
-			</ul>
+			<table class="fontList">
+				<thead>
+					<tr>
+						<th class="font">フォント</th>
+						<th class="weight">ウェイト</th>
+						<th class="type">種類</th>
+						<th class="delete">削除</th>
+					</tr>
+				</thead>
+				<tbody class="fontList"></tbody>
+			</table>
 			<button name="submit" class="add button button-primary">＋フォントを追加</button>
-			<input type="submit" name="submit" class="submit button button-primary" value="変更を保存">
+			<button name="submit" class="save button button-primary">変更を保存</button>
 		</form>';
+	}
+
+	public function save() {
+		$token = stripslashes( $_POST['token'] );
+		$css = stripslashes( $_POST['css'] );
+		$options = $_POST['options'];
+		$jsonArray = [
+			"status" => 0
+		];
+		$data = [
+			"token"   => $token,
+			"css"     => $css,
+			"options" => $options
+		];
+		update_option( $this->_option_name, $data );
+		wp_send_json( $jsonArray );
+	}
+
+	public function load() {
+		$data = get_option( $this->_option_name, true );
+		wp_send_json( $data );
+	}
+
+	public function css() {
+		$data = get_option( $this->_option_name, true );
+		echo '<style>' . $data['css'] . '</style>';
+//		echo '<style>body{font-family: AnzuMojiMono !important;}</style>';
 	}
 }
--- wordpress/wp-content/plugins/font-stream/js/main.js
+++ wordpress/wp-content/plugins/font-stream/js/main.js
@@ -2,66 +2,246 @@
  * Created by taichi on 2017/07/18.
  */
 
-(function($) {
+(function ($) {
     "use strict";
     var listUrl = "/wp-content/plugins/font-stream/list.json";
     var fontList = null;
-    var onFetch = false;
+    var onFetch = true;
+
 
     $(document).ready(function () {
         var $form = $("form.font-stream");
-        var $button = $form.find("button.add");
-        var $fontList = $form.find("ul.fontList");
+        var $buttonAdd = $form.find("button.add");
+        var $buttonSave = $form.find("button.save");
+        var $fontList = $form.find("tbody.fontList");
 
-        var appendFont = function(){
-            console.log(fontList);
-            var $select = $("<select />");
-            for(var i=0; i<fontList.length; i++){
-                var font = fontList[i];
+
+        var createFontOptions = function (data) {
+            var $tr = $('<tr class="row"/>');
+            var $tdFont = $('<td class="font" />');
+            var $tdWeight = $('<td class="weight" />');
+            var $tdType = $('<td class="type" />');
+            var $tdDelete = $('<td class="delete" />');
+            $tr.append($tdFont);
+            $tr.append($tdWeight);
+            $tr.append($tdType);
+            $tr.append($tdDelete);
+
+            var $font = $('<select class="font" />');
+            for (var font in fontList) {
                 var $option = $("<option />");
-                $option.attr("value", i);
-                $option.text(font.fontName);
-                console.log(font.fontName);
-                $select.append($option);
+                $option.attr("value", font);
+                $option.text(font);
+                $font.append($option);
             }
-            var $li = $("<li />");
+
+            $font.on("change", function () {
+                $tdWeight.empty();
+                $tdType.empty();
+                var font = $(this).val();
+                var $weight = getWeightOptions(font, data);
+                var $type = getTypeOptions(font, data);
+
+
+                if ($weight !== null) {
+                    $tdWeight.append($weight);
+                }
+                if ($type !== null) {
+                    $tdType.append($type);
+                }
+
+            });
             var $del = $('<a href="#">削除</a>');
 
-            var $weight = $('<select><option>L</option><option>M</option></select>');
-            var $mp = $('<select><option>等幅</option><option>プロポーショナル</option></select>');
 
-            $del.on('click',function(e){
+            $del.on('click', function (e) {
                 e.preventDefault();
-                $li.remove();
+                $tr.remove();
             });
-            $li.append($select);
-            $li.append($weight);
-            $li.append($mp);
-            $li.append($del);
+            $tdFont.append($font);
+            $tdDelete.append($del);
 
-            $fontList.append($li);
-            onFetch = false;
+            $fontList.append($tr);
+            if (typeof data !== 'undefined' && data.font) {
+                $font.val(data.font);
+            }
+            $font.trigger("change");
+        };
+
+        var getWeightOptions = function (font, data) {
+            var weightList = fontList[font].weight;
+            if (weightList.length === 0) {
+                return null;
+            }
+            var $weight = $('<select class="weight" />');
+            for (var i = 0; i < weightList.length; i++) {
+                var $option = $("<option />");
+                $option.attr("value", weightList[i]);
+                $option.text(weightList[i]);
+                $weight.append($option);
+            }
+            if (typeof data !== 'undefined' && data.weight) {
+                $weight.val(data.weight);
+            }
+            return $weight;
+        };
+
+        var getTypeOptions = function (font, data) {
+            var typeList = fontList[font].type;
+            if (typeList.length === 0) {
+                return null;
+            }
+            var $type = $('<select class="type" />');
+            for (var i = 0; i < typeList.length; i++) {
+                var $option = $("<option />");
+                $option.attr("value", typeList[i]);
+                var text = "";
+                if (typeList[i] === "pro") {
+                    text = "プロポーショナル";
+                } else if (typeList[i] === "mono") {
+                    text = "等幅";
+                }
+                $option.text(text);
+                $type.append($option);
+            }
+            if (typeof data !== 'undefined' && data.type) {
+                $type.val(data.type);
+            }
+            return $type;
         };
 
 
-        $button.on("click", function(e){
-            if(onFetch){
+        $buttonAdd.on("click", function (e) {
+            e.preventDefault();
+            if (onFetch || fontList === null) {//json取得中は何もしない
                 return;
             }
-            onFetch = true;
-            e.preventDefault();
-            if(fontList === null) {
-                $.ajax({
-                    url: listUrl,
-                    dataType: "json"
-                }).done(function (json) {
-                    fontList = json;
-                    appendFont();
-                });
-            } else {
-                appendFont();
-            }
+            createFontOptions();
         });
+
+        var createCss = function (token, options) {
+            var css = "";
+            options.map(function (row) {
+                var props = fontList[row.font];
+                var fontFamily = props.fontFamily;
+
+                if (row.type === 'mono') {
+                    fontFamily += 'Mono';
+                }
+                var weight = "none";
+                var url = props.path + fontFamily;
+                if (row.weight !== null) {
+                    weight = row.weight;
+                    fontFamily = fontFamily + "-" + weight;
+                }
+                url =  url + "-" + weight;
+
+
+
+                css += '@font-face{';
+                css += 'font-family: "' + fontFamily + '";'
+                css += 'font-weight: normal;'
+
+                css += 'src: ';
+
+                props.format.map(function (format) {
+                    var extension = null;
+                    switch (format) {
+                        case "embedded-opentype":
+                            extension = 'eot';
+                            break;
+                        case "woff":
+                            extension = 'woff';
+                            break;
+                        case "truetype":
+                            extension = 'ttf';
+                            break;
+                        case "opentype":
+                            extension = 'otf';
+                            break;
+                        default:
+                            return;
+                    }
+                    css += 'url("https://www.font-stream.com/fontdata/AnzuMojiMono-none.' + extension + '?token=' + token + '") ';
+                    css += 'format("' + format + '"),';
+                });
+
+                css = css.substr(0, css.length - 1) + ";";
+
+                css += '}';
+            });
+            return css;
+        };
+
+        $buttonSave.on("click", function (e) {
+            e.preventDefault();
+            if (onFetch) {//json取得中は何もしない
+                return;
+            }
+            var token = $("#token").val();
+            var $rows = $fontList.find('.row');
+            var options = [];
+            $rows.each(function () {
+                var result = {
+                    "font": null,
+                    "weight": null,
+                    "type": null
+                };
+                var $font = $(this).find('select.font');
+                result.font = $font.val();
+                var $weight = $(this).find('select.weight');
+                if (typeof $weight.val() !== "undefined") {
+                    result.weight = $weight.val();
+                }
+                var $type = $(this).find('select.type');
+                if (typeof $type.val() !== "undefined") {
+                    result.type = $type.val();
+                }
+                options.push(result);
+            });
+            var css = createCss(token, options);
+            var endpoint = FONT_STREAM.endpoint + "?action=font_stream_save";
+            $.ajax({
+                url: endpoint,
+                dataType: "json",
+                method: "POST",
+                data: {
+                    token: token,
+                    css: css,
+                    options: options
+                },
+            }).done(function (json) {
+                alert("更新完了");
+                console.log(json);
+            }).fail(function () {
+                alert("保存に失敗しました。");
+            });
+
+        });
+
+        $.ajax({
+            url: listUrl,
+            dataType: "json"
+        }).done(function (json) {
+            fontList = json;
+            onFetch = false;
+            var endpoint = FONT_STREAM.endpoint + "?action=font_stream_load";
+            $.ajax({
+                url: endpoint,
+                dataType: "json"
+            }).done(function (json) {
+                console.log(json);
+                $("#token").val(json.token);
+
+                for (var i = 0; i < json.options.length; i++) {
+                    createFontOptions(json.options[i]);
+                }
+
+
+            });
+        });
+
+
     });
 })(jQuery);
 
